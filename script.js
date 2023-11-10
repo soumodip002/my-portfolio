b5738dc74c570a5c184214b30d536a961f1de198
Soumadip Ghosh  Chowdhury <soumodipghoshchowdhury912@gmail.com>
2023-11-10 08:59:17 -0800

    update

---
+++ script.js
@@ -0,0 +1,94 @@
+// ----------togglr navbar----------
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", () =>{
+    hideSection();
+    toggleNavbar();
+    document.body.classList.toggle("hide-scrolling");
+});
+function hideSection(){
+    // document.querySelector("section.active").classList.toggle("fade-out");
+    document.querySelector("div.active").classList.toggle("fade-out");
+}
+function toggleNavbar(){
+    document.querySelector(".header").classList.toggle("active");
+
+}
+
+
+
+// ----------acctive section----------
+document.addEventListener("click", (e) =>{
+    if(e.target.classList.contains("link-item") && e.target.hash !== ""){
+        // activate the overlay to multiple clicks
+        document.querySelector(".overlay").classList.add("active");
+        navToggler.classList.add("hide");
+        if(e.target.classList.contains("nav-item")){
+            toggleNavbar();
+        }
+        else{
+            hideSection();
+            document.body.classList.add("hide-scrolling");
+        }
+        setTimeout(() =>{
+            document.querySelector("div.active").classList.remove("active","fade-out");
+            document.querySelector(e.target.hash).classList.add("active");
+            window.scrollTo(0,0);
+            document.body.classList.remove("hide-scrolling");
+            navToggler.classList.remove("hide");
+            document.querySelector(".overlay").classList.remove("active");
+        },500);
+    }
+});
+
+// ----------about tabs----------
+const tabsContainer = document.querySelector(".about-tabs"),
+// aboutSection = document.querySelector(".about-selection");
+aboutSection = document.querySelector(".about-section");
+
+tabsContainer.addEventListener("click", (e) =>{
+    if(e.target.classList.contains("tab-item") && !e.target.classList.contains("active")){
+        tabsContainer.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+        const target = e.target.getAttribute("data-target");
+        aboutSection.querySelector(".tab-content.active").classList.remove("active");
+        aboutSection.querySelector(target).classList.add("active");
+        
+    }
+});
+
+// ---------portfolio item details popup----------
+document.addEventListener("click",(e) =>{
+    if(e.target.classList.contains("view-work-btn")){
+        toggleportfoliopopup();
+        document.querySelector(".portfolip-popup").scrollTo(0,0);
+        portfolioitemdetails(e.target.parentElement);
+    }
+})
+function toggleportfoliopopup(){
+    document.querySelector(".portfolio-popup").classList.toggle("open");
+    document.body.classList.toggle("hide-scrolling");
+    document.querySelector(".main").classList.toggle("fade-out");
+}
+document.querySelector(".pp-close").addEventListener("click",toggleportfoliopopup);
+// -----hide popup when clicking outside of it-----
+document.addEventListener("click", (e) =>{
+    if(e.target.classList.contains("pp-inner")){
+        toggleportfoliopopup();
+        // document.querySelector(".portfolip-popup").scrollTo(0,0);
+        // document.querySelector(".main").classList.toggle("fade-out")
+    }
+
+});
+
+function portfolioitemdetails(portfolioitem){
+    document.querySelector("pp-thumbnail img").src =
+    portfolioitem.querySelector(".portfolio-item-thumbnail img").src;
+
+    document.querySelector(".pp-header h3").innerHTML =
+    portfolioitem.querySelector(".portfolio-item-title").innerHTML;
+
+    document.querySelector(".pp-body").innerHTML =
+    portfolioitem.querySelector(".portfolip-item-details").innerHTML;
+
+}
+
